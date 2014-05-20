d0152e6222e9556f935920e7cf746852bb282713
KonstantinViktorovich <ooo111oooo1111@yandex.ru>
2014-05-20 15:18:03 +0700

    заглушка

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -14,8 +14,8 @@ public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        //resp.getWriter().print("Hello from Java!\n");
-		
+        resp.getWriter().print("Hello from Java!\n");
+		/*
 		Statement stmt = null;
 	    ResultSet rs;
 		try {
@@ -27,7 +27,7 @@ public class HelloWorld extends HttpServlet {
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
+		}*/
 		
     }
 
